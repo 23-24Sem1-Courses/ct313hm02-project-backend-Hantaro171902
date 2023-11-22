59d04b866f5a4e60c3d45f441529bcf7e94fd027
Hantaro191702 <hanb2014913@student.ctu.edu.vn>
2023-11-22 20:41:03 +0700

    updated by Han

--- src/database/sql/coffee_shop.sql
+++ src/database/sql/coffee_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 08, 2023 at 04:43 AM
+-- Generation Time: Nov 22, 2023 at 02:39 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -20,30 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `coffee_shop`
 --
-CREATE DATABASE IF NOT EXISTS `coffee_shop` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `coffee_shop`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cf_cart_items`
---
-
-CREATE TABLE `cf_cart_items` (
-  `cart_id` int(11) NOT NULL,
-  `session_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `cf_cart_items`
---
-
-INSERT INTO `cf_cart_items` (`cart_id`, `session_id`, `product_id`, `quantity`, `created_at`) VALUES
-(10, 1, 1, 1, '2023-11-08 03:32:09'),
-(11, 1, 2, 1, '2023-11-08 03:32:41');
 
 -- --------------------------------------------------------
 
@@ -87,28 +63,20 @@ CREATE TABLE `cf_drinks` (
 INSERT INTO `cf_drinks` (`dr_id`, `dr_name`, `dr_price`, `dr_img`, `cate_id`) VALUES
 (1, 'Hot Americano', 55000, 'AMERICANO_NONG.jpg', 1),
 (2, 'Bac Xiu', 25000, 'BAC_XIU.jpg', 1),
-(3, 'Tra Sen Vang ', 45000, 'TRA_SEN_VANG_CU_NANG.jpg', 2);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cf_shopping_session`
---
-
-CREATE TABLE `cf_shopping_session` (
-  `session_id` int(255) NOT NULL,
-  `user_id` varchar(100) NOT NULL,
-  `total` decimal(10,0) NOT NULL DEFAULT 0,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `cf_shopping_session`
---
-
-INSERT INTO `cf_shopping_session` (`session_id`, `user_id`, `total`, `created_at`) VALUES
-(1, '100', '100000', '2023-11-08 03:30:42'),
-(2, '100', '100000', '2023-11-08 03:31:16');
+(3, 'Tra Sen Vang ', 45000, 'TRA_SEN_VANG_CU_NANG.jpg', 2),
+(4, 'Freeze So-co-la', 55000, 'FREEZE_CHOCO.jpg', 4),
+(5, 'Banh So-co-la Highlands', 35000, 'SOCOLAHL.png', 3),
+(6, 'Banh Caramel Pho Mai', 35000, 'CARAMELPHOMAI.jpg', 3),
+(7, 'Banh Pho Mai Chanh Day', 29000, 'PHOMAICHANHDAY.jpg', 3),
+(8, 'Tra Qua Mong Anh Dao', 59000, 'TEA_BERRY.jpg', 2),
+(9, 'Tra Xanh Dau Do', 45000, 'TRA_XANH_DAU_DO.jpg', 2),
+(10, 'Cookies & Cream', 55000, 'COOKIES_FREEZE.jpg', 4),
+(11, 'Phinidi Choco', 45000, 'PHINDI_CHOCO.jpg', 1),
+(12, 'Tra Thach Vai', 45000, 'TRA_THACH_VAI', 2),
+(13, 'Mocha Macchiato', 69000, 'MOCHA_MACCHIATO.jpg', 1),
+(14, 'Caramel Freeze Phindi', 55000, 'CARAMEL_FREEZE_PHINDI.jpg', 4),
+(16, 'Hot Americano', 55000, 'AMERICANO_NONG.jpg', 1),
+(17, 'Hot Americano', 55000, 'AMERICANO_NONG.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -117,10 +85,9 @@ INSERT INTO `cf_shopping_session` (`session_id`, `user_id`, `total`, `created_at
 --
 
 CREATE TABLE `cf_user` (
-  `u_id` varchar(100) NOT NULL,
+  `u_id` int(100) UNSIGNED NOT NULL,
   `u_name` varchar(50) NOT NULL,
   `u_password` int(11) NOT NULL,
-  `u_role` int(5) NOT NULL,
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `u_address` varchar(100) NOT NULL,
@@ -132,21 +99,32 @@ CREATE TABLE `cf_user` (
 -- Dumping data for table `cf_user`
 --
 
-INSERT INTO `cf_user` (`u_id`, `u_name`, `u_password`, `u_role`, `first_name`, `last_name`, `u_address`, `u_telephone`, `u_email`) VALUES
-('100', 'admin', 123456, 0, 'han', 'chau', 'Can Tho', 708091645, 'hanb2014913@student.ctu.edu.vn'),
-('101', 'user', 123456, 1, 'gia', 'chau', 'Can Tho', 123456789, 'chaugiahan001@gmail.com');
+INSERT INTO `cf_user` (`u_id`, `u_name`, `u_password`, `first_name`, `last_name`, `u_address`, `u_telephone`, `u_email`) VALUES
+(1, 'Jake_Smitham', 0, 'Tristian', 'Dietrich', 'New Rochelle', 952889254, 'Lee3@gmail.com'),
+(2, 'Hazel_Shields', 0, 'Hilbert', 'Little', 'Fort Baileehaven', 922326697, 'Mercedes.Schamberger39@gmail.com'),
+(3, 'Savion34', 0, 'Christine', 'Marvin', 'Marysville', 948543294, 'Brennan_Wolff74@hotmail.com'),
+(4, 'Karlee77', 0, 'Mariah', 'Douglas', 'Fort Blazeburgh', 944096822, 'Olen.Rippin-Kiehn1@hotmail.com'),
+(5, 'Katlynn74', 0, 'Myron', 'Herzog', 'Mertzburgh', 904966734, 'Paige37@hotmail.com'),
+(6, 'Ashleigh.Legros45', 0, 'Henderson', 'Altenwerth', 'Napa', 961539958, 'Lucy86@yahoo.com'),
+(7, 'Frieda_Veum-Gislason9', 0, 'Juvenal', 'Waelchi', 'Callieton', 998252041, 'Dan74@gmail.com'),
+(8, 'Eugenia40', 0, 'Hosea', 'Crooks', 'Cypress', 907310236, 'Reed.Effertz@gmail.com'),
+(9, 'Orpha.Roob', 0, 'Janie', 'Waters', 'Brekkeport', 975375736, 'Cary_Leffler@hotmail.com'),
+(10, 'Rafael88', 0, 'Raymundo', 'Becker', 'New Camryn', 915888135, 'Joaquin5@yahoo.com'),
+(11, 'Jake_Smitham', 0, 'Tristian', 'Dietrich', 'New Rochelle', 952889255, 'Lee3@gmail.com'),
+(12, 'Hantaro', 123, 'Han', 'Chau', 'Can Tho', 708091645, 'hanB2014913@gmail.com'),
+(13, 'Test', 0, 'Tristian', 'Dietrich', 'New Rochelle', 952889254, 'Lee3@gmail.com'),
+(14, '', 0, '', '', '', 0, ''),
+(15, '', 0, '', '', '', 0, ''),
+(16, '', 0, '', '', '', 0, ''),
+(17, '', 0, '', '', '', 0, ''),
+(18, '', 0, '', '', '', 0, ''),
+(19, '', 0, '', '', '', 0, ''),
+(20, '', 0, '', '', '', 0, ''),
+(21, '', 0, '', '', '', 0, '');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `cf_cart_items`
---
-ALTER TABLE `cf_cart_items`
-  ADD PRIMARY KEY (`cart_id`),
-  ADD KEY `session_id` (`session_id`),
-  ADD KEY `product_id` (`product_id`);
 
 --
 -- Indexes for table `cf_categories`
@@ -162,57 +140,36 @@ ALTER TABLE `cf_drinks`
   ADD KEY `cate_id` (`cate_id`);
 
 --
--- Indexes for table `cf_shopping_session`
---
-ALTER TABLE `cf_shopping_session`
-  ADD PRIMARY KEY (`session_id`),
-  ADD KEY `user_id` (`user_id`);
-
---
 -- Indexes for table `cf_user`
 --
 ALTER TABLE `cf_user`
-  ADD PRIMARY KEY (`u_id`),
-  ADD UNIQUE KEY `u_name` (`u_name`);
+  ADD PRIMARY KEY (`u_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `cf_cart_items`
+-- AUTO_INCREMENT for table `cf_drinks`
 --
-ALTER TABLE `cf_cart_items`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+ALTER TABLE `cf_drinks`
+  MODIFY `dr_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
--- AUTO_INCREMENT for table `cf_shopping_session`
+-- AUTO_INCREMENT for table `cf_user`
 --
-ALTER TABLE `cf_shopping_session`
-  MODIFY `session_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `cf_user`
+  MODIFY `u_id` int(100) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `cf_cart_items`
---
-ALTER TABLE `cf_cart_items`
-  ADD CONSTRAINT `cf_cart_items_ibfk_1` FOREIGN KEY (`session_id`) REFERENCES `cf_shopping_session` (`session_id`),
-  ADD CONSTRAINT `cf_cart_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `cf_drinks` (`dr_id`);
-
---
 -- Constraints for table `cf_drinks`
 --
 ALTER TABLE `cf_drinks`
   ADD CONSTRAINT `cf_drinks_ibfk_1` FOREIGN KEY (`cate_id`) REFERENCES `cf_categories` (`cate_id`);
-
---
--- Constraints for table `cf_shopping_session`
---
-ALTER TABLE `cf_shopping_session`
-  ADD CONSTRAINT `cf_shopping_session_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `cf_user` (`u_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- src/services/drinks.service.js
+++ src/services/drinks.service.js
@@ -2,110 +2,115 @@ const knex = require("../database/knex");
 const Paginator = require("./paginator");
 
 function makeDrinksService() {
-    function readDrink(payload) {
-        const drink = {
-            dr_name: payload.dr_name,
-            dr_price: payload.dr_price,
-            dr_img: payload.dr_img,
-            cate_id: payload.cate_id,
-        };
-        Object.keys(drink).forEach(
-            (key) => drink[key] === undefined && delete drink[key]
-        );
-        return drink;
-    }
+  function readDrink(payload) {
+    const drink = {
+      dr_name: payload.dr_name,
+      dr_price: payload.dr_price,
+      dr_img: payload.dr_img,
+      cate_id: payload.cate_id,
+    };
+    Object.keys(drink).forEach(
+      (key) => drink[key] === undefined && delete drink[key]
+    );
+    return drink;
+  }
 
-    function readCategories(payload) {
-        const cate_drink = {
-            cate_id: payload.cate_id,
-            cate_name: payload.cate_name,
-        };
+  function readCategories(payload) {
+    const cate_drink = {
+      cate_id: payload.cate_id,
+      cate_name: payload.cate_name,
+    };
 
-        Object.keys(cate_drink).forEach(
-            (key) => cate_drink[key] === undefined && delete cate_drink[key]
-        );
-        return cate_drink;
-    }
+    Object.keys(cate_drink).forEach(
+      (key) => cate_drink[key] === undefined && delete cate_drink[key]
+    );
+    return cate_drink;
+  }
 
-    async function createDrink(payload) {
-        const drink = readDrink(payload);
-        const [id] = await knex("cf_drinks").insert(drink);
-        return { id, ...drink };
-    }
+  async function createDrink(payload) {
+    const drink = readDrink(payload);
+    const [id] = await knex("cf_drinks").insert(drink);
+    return { id, ...drink };
+  }
 
-    async function createCategory(payload) {
-        const cate_drink = readCategories(payload);
-        const [id] = await knex("cf_categories").insert(cate_drink);
-        return { id, ...cate_drink };
-    }
+  async function createCategory(payload) {
+    const cate_drink = readCategories(payload);
+    const [id] = await knex("cf_categories").insert(cate_drink);
+    return { id, ...cate_drink };
+  }
 
-    async function getManyDrinks(query) {
-        const { dr_name, page = 1, limit = 10 } = query;
-        const paginator = new Paginator(page, limit);
-        let results = await knex("cf_drinks")
-            .join("cf_categories", "cf_drinks.cate_id", "=", "cf_categories.cate_id")
-            .where((builder) => {
-                if (dr_name) {
-                    builder.where("dr_name", "like", `%${dr_name}%`);
-                }
-            })
-            .select(
-                knex.raw("count(dr_id) OVER() AS recordsCount"),
-                "dr_id",
-                "dr_name",
-                "dr_price",
-                "dr_img",
-                "cf_categories.cate_name"
-            )
-            .limit(paginator.limit)
-            .offset(paginator.offset);
-        let totalRecords = 0;
-        results = results.map((result) => {
-            totalRecords = result.recordsCount;
-            delete result.recordsCount;
-            return result;
-        });
-        return {
-            metadata: paginator.getMetadata(totalRecords),
-            drink: results,
-        };
-    }
+  async function getManyDrinks(query) {
+    const { dr_name, page = 1, limit = 10 } = query;
+    const paginator = new Paginator(page, limit);
+    let results = await knex("cf_drinks")
+      .join("cf_categories", "cf_drinks.cate_id", "=", "cf_categories.cate_id")
+      .where((builder) => {
+        if (dr_name) {
+          builder.where("dr_name", "like", `%${dr_name}%`);
+        }
+      })
+      .select(
+        knex.raw("count(dr_id) OVER() AS recordsCount"),
+        "dr_id",
+        "dr_name",
+        "dr_price",
+        "dr_img",
+        "cf_categories.cate_name"
+      )
+      .limit(paginator.limit)
+      .offset(paginator.offset);
+    let totalRecords = 0;
+    results = results.map((result) => {
+      totalRecords = result.recordsCount;
+      delete result.recordsCount;
+      return result;
+    });
 
-    async function getManyCategories() {
-        return knex("cf_categories").select("*");
-    }
-
-    async function getDrinkById(id) {
-        return knex("cf_drinks").where("dr_id", id).select("*").first();
-    }
-
-    async function updateDrink(id, payload) {
-        const update = readDrink(payload);
-        return knex("cf_drinks").where("dr_id", id).update(update);
-    }
-
-    async function deleteDrink(id) {
-        return knex("cf_drinks").where("dr_id", id).del();
-    }
-
-    async function deleteCategory(id) {
-        return knex("cf_categories").where("cate_id", id).del();
-    }
-
-    async function deleteAllDrinks() {
-        return knex("cf_drinks").del();
-    }
+    // Retrieve distinct categories
+    const categories = await knex("cf_categories").distinct("cate_name");
 
     return {
-        createDrink,
-        getManyDrinks,
-        getDrinkById,
-        updateDrink,
-        deleteDrink,
-        deleteAllDrinks,
-        createCategory,
-        getManyCategories,
-        deleteCategory,
+      metadata: paginator.getMetadata(totalRecords),
+      drink: results,
+      categories: categories.map((category) => category.cate_name),
     };
+  }
+
+  async function getManyCategories() {
+    return knex("cf_categories").select("*");
+  }
+
+  async function getDrinkById(id) {
+    return knex("cf_drinks").where("dr_id", id).select("*").first();
+  }
+
+  async function updateDrink(id, payload) {
+    const update = readDrink(payload);
+    return knex("cf_drinks").where("dr_id", id).update(update);
+  }
+
+  async function deleteDrink(id) {
+    return knex("cf_drinks").where("dr_id", id).del();
+  }
+
+  async function deleteCategory(id) {
+    return knex("cf_categories").where("cate_id", id).del();
+  }
+
+  async function deleteAllDrinks() {
+    return knex("cf_drinks").del();
+  }
+
+  return {
+    createDrink,
+    getManyDrinks,
+    getDrinkById,
+    updateDrink,
+    deleteDrink,
+    deleteAllDrinks,
+    createCategory,
+    getManyCategories,
+    deleteCategory,
+  };
 }
 module.exports = makeDrinksService;
